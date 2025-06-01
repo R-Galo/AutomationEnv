0ee8c0eff72f5b7fa03844e1ef7af054b43561eb
R-Galo <ruddy.galo@hotmail.com>
2025-06-01 12:31:29 -0600

    Added variables explanation
    Added best practices for managing sensitive data and preventing risks of leaking it

--- config/config.ts
+++ config/config.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
-dotenv.config(); // Loading environment variables from .env
+dotenv.config(); // Loading environment variables from .env you should create .env in your project to add sensitive information and preventing a breach of security
 
 export const testConfig = {
     baseUrl: 'https://ecommerce-playground.lambdatest.io/index.php?route=account/login',
-    firstName: process.env.TEST_FIRST_NAME!,
+    firstName: process.env.TEST_FIRST_NAME!, //This information is pulled from .env doc you can set your own information here *NOT RECOMMENDED: unless you add this doc to .gitignore*, or create a .env doc to add sensitive information there and import the data from here 
     lastName: process.env.TEST_LAST_NAME!,
     email: process.env.TEST_EMAIL!,
     password: process.env.TEST_PASSWORD!
